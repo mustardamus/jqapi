1ef4b9d3f86ec74965934d2ee01e2cd395be9761
Sebastian Senf <contact@mustardamus.com>
2010-01-30 15:34:05 +0100

    load navigation, resizable layout

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-  
+  $('#navigation').load('navigation.html', function() { //load the navigation (not static because it gets generated with the api scraping)
+    
+    
+    var search_el = $('#search');
+    var content_el = $('#content');
+    var static_el = $('#static-list');
+    var searchHeight = search_el.height();
+
+    function resizeLayout() {
+      var winh = $(window).height();
+      
+      static_el.height(winh - searchHeight);
+      content_el.height(winh);
+    }
+
+    resizeLayout();
+    $(window).resize(function() { resizeLayout(); });
+    
+    
+    
+  });
 });
