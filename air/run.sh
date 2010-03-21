21ab9f31dac8dfe81eb93030517f1aae8fc39c2c
erikzaadi <Erik@erikr2.xmpie.com>
2010-03-21 18:22:04 +0200

    added scripts for mac/linux

---
+++ air/run.sh
@@ -0,0 +1,4 @@
+#REM Prepend the "adl" part with path to your installed air sdk bin folder (http://www.adobe.com/products/air/tools/sdk/)
+echo jQAPI Air Offline application
+echo running application...
+adl -pubid 3E5C46042CDA5A1F9AA301B445CE43A5D5223395 ./application.xml ./../
