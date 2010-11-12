fb518ac4b0c7aed44e31800699ef637eebbeb29f
erikzaadi <erik.zaadi@gmail.com>
2010-11-12 20:32:53 +0200

    Updated to 1.4.4

--- air/build.bat
+++ air/build.bat
@@ -4,4 +4,4 @@ echo jQAPI Air Offline application
 echo ****************************  
 echo building...
 
-adt -package -storetype pkcs12 -keystore jQAPI.p12 jQAPI-1.4.2.air ./application.xml -C ./../ css docs images js lib blank.html favicon.ico index.html navigation.html air/loader.html air/images
+adt -package -storetype pkcs12 -keystore jQAPI.p12 jQAPI-1.4.4.air ./application.xml -C ./../ css docs images js lib blank.html favicon.ico index.html navigation.html air/loader.html air/images
