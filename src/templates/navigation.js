781969055cdcee8d01a8fe501be3addd3adbb680
Sebastian Senf <contact@mustardamus.com>
2016-09-04 15:40:21 +0200

    removed long category desc from navigation

--- src/templates/navigation.js
+++ src/templates/navigation.js
@@ -6,7 +6,6 @@ module.exports = {
     return `
       <li class="category ${category.slug}">
         <div class="category-name">${category.name}</div>
-        <div class="category-desc">${category.desc}</div>
       </li>
     `
   },
